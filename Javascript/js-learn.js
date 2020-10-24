e1d618086821601cc6e534ff652ac21d171433a1
ericxyan <eric.yan828@gmail.com>
2020-10-23 21:36:16 -0400

    update

---
+++ Javascript/js-learn.js
@@ -0,0 +1,63 @@
+// String
+const str = 'hello';
+str.length // 5
+str[0] // 'h'
+str.charAt(0) // 'h'
+str.indexOf('l') // 2
+str.lastIndexOf('l') // 3
+str.startsWith('h') // true
+str.endsWith('o') // true
+str.includes('el') // true
+str.split('l') // ['he', '', 'o']
+str.slice(0) // 'ello'
+str.slice(1,3) // 'el'
+str.substring(0) // = slice(0)
+str.replace('ll', 'l') 'helo'
+str.match(/.llo/)
+str.toUpperCase()
+str.repeat(2)
+`${str} world`
+
+
+
+// Array
+// Indexes are 0-based
+// Create
+let arr = [1, 2];
+arr = new Array(1, 2);
+arr = Array(1, 2);
+arr = Array.of(1, 2);
+
+arr = new Array(2);
+arr.length // 2
+arr[1] // undefined
+
+arr[9] // undefined
+arr.length // 10
+
+// Read
+arr = ['a', 'b']
+arr[0] // 'a'
+arr[9] // undefined, no Out of Index
+// iterate
+for(a of arr) { console.log(a) } // a, b
+for(a in arr) { console.log(a) } // 1, 2
+arr.forEach(console.log)
+arr.map(a => a + 1) // [a1, b1]
+// search
+arr.includes(e) // true
+arr.indexOf('a') // 0, or -1 not found
+arr.find(e => e == 'a') // 'a'
+arr.filter(e => e == 'a') // ['a']
+arr.findIndex(e => e == 'a') // 0
+
+// Add
+arr.push('c', 'd'); // add to end
+arr.unshift('-1', '-2'); // add to front, ['-1', '-2', 'a', 'b', 'c', 'd']
+
+
+// Delete
+
+arr.push(3); // [1,2,3]
+arr.pop(); // 3, arr = [1, 2]
+arr.shift(); // 1, arr = [2]
